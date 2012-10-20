8354ae4851986a36a82ef5ce6d99afbcf10f15b6
Raghav Mohan <raghav.mohan@gmail.com>
2012-10-19 21:48:29 -0500

    Init Commit

---
+++ Greasy2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greasy2::Application.config.secret_token = 'fc5369ee23d75ac265876d35a9c5161fd2e3ff28e69fa698c993b5e811b3b7b198adb84660df600075f8c85591aa1225458d4aa3e85877408dc23b50fa2855d5'
