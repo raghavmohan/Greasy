7cb43fe4dbc41b423e64d0a099779a6d5b578d0e
Raghav Mohan <raghav.mohan@gmail.com>
2012-10-19 19:55:53 -0500

    Created App, nothing in it yet

---
+++ Greasy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greasy::Application.config.secret_token = '09fd969239d594852e1b70b707fd85be4543c8196b38947a7bb89650d6d3095a11ce3215e0ebc5b6700d1a24f2759b14b126219247992bdc2c8510743c0319c8'
