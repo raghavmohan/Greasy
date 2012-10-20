03c475842fddeeba431a3df18f02274d78dfb8e7
Justin Starry <jstarry@wisc.edu>
2012-10-20 02:50:27 -0500

    Updated Presentation View

--- assets/js/greasy.js
+++ assets/js/greasy.js
@@ -1,7 +1,51 @@
+var menuToggled = false;
+var slideOrderToggled = false;
+
 $(document).keydown(function(event) {
 	if (event.which == 37) {
 		$('.carousel').carousel('prev')
-	} else if (event.which == 39) {
+	} else if (event.which == 39 || event.which == 32 || event.which == 13) {
 		$('.carousel').carousel('next')
 	}
 });
+
+$("#menu-handle").click(function() {
+	if (!menuToggled) {
+		menuToggled = true;
+		$('#menu').animate({left: '+=100'}, 300, function() { 
+			$('#menu-handle').animate({opacity: '1.0'}, 200, function() {});
+		});
+	} else {
+		menuToggled = false;
+		$('#menu').animate({left: '-=100'}, 300, function() {
+			$('#menu-handle').animate({opacity: '0.5'}, 200, function() {});
+		});
+	}
+});
+
+$("#menu-handle").hover(function() {
+		if (!menuToggled) {
+			$('#menu-handle').animate({opacity: '1.0'}, 200, function() {});
+		}
+	},
+	function() {
+		if (!menuToggled) {
+			$('#menu-handle').animate({opacity: '0.5'}, 200, function() {});
+		}
+	}
+);
+
+$("button#exit").click(function() {
+	window.location = 'userpresentations.html';
+});
+
+$("button#slides").click(function() {
+	if (!slideOrderToggled) {
+		slideOrderToggled = true;
+		$("#menu").animate({left: '+=400'}, 300, function() {});
+	} else {
+		slideOrderToggled = false;
+		$("#menu").animate({left: '-=400'}, 300, function() {});
+	}
+});
+
